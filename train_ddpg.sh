a4317569cd45489a15fcee36fcd35867d4db651b
OwenZhu <398094982@qq.com>
2018-07-01 17:07:24 +1000

    update train ddpg sh

--- train_ddpg.sh
+++ train_ddpg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for n in $( seq 1 10 )
+for num in 2 4 6 8 10
 do
-    for i in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+    for w in 0.0 0.2 0.4 0.6 0.8 1.0
         do
-            python ddpg_main_loop.py --n_agents=${n} --sustainable_weight=${i}
+            python ddpg_main_loop.py --n_agents=${num} --sustainable_weight=${w}
         done
 done
