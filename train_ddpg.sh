51900cf631fe6ce9f9745af9ef79c048dd01bbb2
OwenZhu <398094982@qq.com>
2018-08-27 12:00:17 +0000

    change parameters

--- train_ddpg.sh
+++ train_ddpg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd agents/
-for num in 6 8 10
+for num in 1 2 4 6 8 10
 do
-    for w in 0.2 0.4 0.6 0.8 1.0
+    for w in 0.0 0.2 0.4 0.6 0.8 1.0
         do
             python ddpg_agent.py --n_agents=${num} --sustain_weight=${w}
         done
