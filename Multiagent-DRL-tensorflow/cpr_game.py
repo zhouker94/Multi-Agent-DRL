37745c7451af250d989607b3874129fc3069b18f
OwenZhu <zhuhanweiowen@gmail.com>
2019-11-23 20:43:17 +1100

    refactor game

--- Multiagent-DRL-tensorflow/cpr_game.py
+++ Multiagent-DRL-tensorflow/cpr_game.py
@@ -5,32 +5,46 @@
 # @File    : environment.py
 
 
-class CPRGame(object):
-    def __init__(self, conf):
-        self.W = conf["sustainable_weight"]
-        self.RG = conf["replenishment_rate"]
-        self.MRC = conf["max_resource_capacity"]
-        self.ALPHA = conf["alpha"]
-        self.BETA = conf["beta"]
-        self.COST = conf["cost"]
+def simple_step_function(X):
+    if X > 0:
+        return 1
+    elif X == 0:
+        return 0
+    else:
+        return -1
 
-        self.pool = self.MRC
+
+class CPRGame:
+    """CPR game
+    """
+
+    def __init__(self, conf):
+        self.spec = {
+            "W": conf["sustainable_weight"],  # weight of sustainability
+            "RG": conf["replenishment_rate"],  # replenishment rate
+            "MRC": conf["max_resource_capacity"],  # maximum resource capacity
+            "ALPHA": conf["alpha"],  # parameter alpha
+            "BETA": conf["beta"],  # parameter beta
+            "COST": conf["cost"],  # cost
+        }
+        self.pool = conf["max_resource_capacity"]
 
     def growth(self, N):
-        return self.RG * N * (1 - N / self.MRC)
+        return self.spec["RG"] * N * (1 - N / self.spec["MRC"])
 
     def harvest(self, x, N):
-        return self.BETA * (x ** self.ALPHA) \
-               * (N ** (1 - self.ALPHA))
+        return self.spec["BETA"] * (x ** self.spec["ALPHA"]) \
+            * (N ** (1 - self.spec["ALPHA"]))
 
-    def reward(self, delta_n, pis):
+    def reward(self, delta_n, pis, step_func=simple_step_function):
         rewards = []
-        # _lambda -> sustainability_goal
-        _lambda = simple_step_function(delta_n)
+        # lambda_ -> sustainability_goal
+        lambda_ = step_func(delta_n)
         # xi -> wealth_goal
         for pi in pis:
-            xi = simple_step_function(pi)
-            rewards.append(self.W * _lambda + (1 - self.W) * xi)
+            xi = step_func(pi)
+            rewards.append(self.spec["W"] * lambda_ +
+                           (1 - self.spec["W"]) * xi)
 
         return rewards
 
@@ -43,7 +57,7 @@ class CPRGame(object):
         game_is_done = True if self.pool <= 5.0 else False
 
         # get feedback from env
-        pis = [x / float(X) * harvest_level - self.COST * x \
+        pis = [x / float(X) * harvest_level - self.spec["COST"] * x
                for x in xs]
         PI = sum(pis)
 
@@ -53,13 +67,4 @@ class CPRGame(object):
         return obs, self.reward(delta_n, pis), game_is_done
 
     def reset(self):
-        self.pool = self.MRC
-
-
-def simple_step_function(X):
-    if X > 0:
-        return 1
-    elif X == 0:
-        return 0
-    else:
-        return -1
+        self.pool = self.spec["MRC"]
